ef74a2de3d56257e002151370c040955f6782953
Haoxuan Yang <427725700@qq.com>
2020-01-22 23:43:17 -0700

    first commit

---
+++ src/constants.js
@@ -0,0 +1,19 @@
+
+export const TOKEN_KEY = 'TOKEN_KEY';
+// export const API_ROOT = "https://around-dot-around-179500.appspot.com/api/v1";
+export const API_ROOT = "https://cors-anywhere.herokuapp.com/http://35.188.114.67:8080/";
+// export const API_ROOT = "http://35.188.114.67:8080/";
+export const GEO_OPTIONS = {
+   enableHighAccuracy: true,
+   maximumAge        : 300000,
+   timeout           : 27000,
+};
+export const POS_KEY = 'POS_KEY';
+export const AUTH_HEADER = 'Bearer';
+
+export const POST_TYPE_IMAGE = 'image';
+export const POST_TYPE_VIDEO = 'video';
+export const POST_TYPE_UNKNOWN = 'unknown';
+
+export const TOPIC_AROUND = 'around';
+export const TOPIC_FACE = 'face';
